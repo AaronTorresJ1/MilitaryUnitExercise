7382c0243ca09f1af1b52d9b00e24afa21de7f38
Aaron Torres <torresaaronj@gmail.com>
2018-05-04 21:50:28 -0400

    Add project files.

---
+++ MilitaryUnitExercise/Military.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryUnitExercise
+{
+    public class Military
+    {
+        public string Name { get; set; }
+
+        public Military(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Mission()
+        {
+            Console.WriteLine("We are going to war");
+        }
+
+        public virtual string StateMission()
+        {
+            return "Defend our country";
+        }
+    }
+
+    public class Marines : Military
+    {
+        public Marines(string name) : base(name)
+        {
+            this.Name = name;
+        }
+
+        public new void Mission()
+        {
+            Console.WriteLine("Marines are first to fight!");
+        }
+
+        public override string StateMission()
+        {
+            return "Marines are first to fight and love to eat crayons...YUT";
+        }
+    }
+
+    public class Army : Military
+    {
+        public Army(string name) : base(name)
+        {
+            this.Name = name;
+        }
+
+        public override string StateMission()
+        {
+            return "The army waits for the Marines.";
+        }
+
+      
+    }
+
+    public class Navy : Military
+    {
+        public Navy(string name) : base(name)
+        {
+            this.Name = name;
+        }
+        public override string StateMission()
+        {
+            return "The Navy takes Marines wherever the mission is";
+        }
+   
+    }
+    public class AirForce : Military
+    {
+        public AirForce(string name) : base(name)
+        {
+            this.Name = name;
+        }
+        public override string StateMission()
+        {
+            return "The Air Force has it made.";
+        }
+      
+    }
+}
---
+++ MilitaryUnitExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MilitaryUnitExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The military has 4 branches with very specific tasks, press ENTER to find out");
+            Console.ReadLine();
+            Marines marines = new Marines("");
+            Army army = new Army("");
+            Navy navy = new Navy("");
+            AirForce airForce = new AirForce("");
+
+            SayMission(marines);
+            Console.WriteLine(marines.StateMission());
+            SayMission(army);
+            Console.WriteLine(army.StateMission());
+            SayMission(navy);
+            Console.WriteLine(navy.StateMission());
+            SayMission(airForce);
+            Console.WriteLine(airForce.StateMission());
+
+        }
+
+        public static void SayMission(Military branch)
+        {
+           
+        }
+    }
+}
